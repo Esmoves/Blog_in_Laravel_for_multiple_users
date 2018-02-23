10dba2973bfb2cc1990d993225d899d1f0880f6a
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-02-23 17:13:01 +0100

    seeds

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,22 +2,32 @@
 
 use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Author::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->name,
+        'firstname' => $faker->firstname,
+	    'lastname' => $faker->lastname,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'password' => $faker->password
+       // 'remember_token' => str_random(10)
+    ];
+});
+
+$factory->define(App\Blog::class, function (Faker $faker) {
+    return [
+        'titel' => $faker->sentence,
+        'excerp' => $faker->paragraph(1),
+        'body' => $faker->text,
+        'image' => $faker->word,
+        'author_id' => function() {
+            return factory(App\Author::class)->create()->id;
+        },
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word
     ];
 });
