8ced0b015a65933f1c781c0d3e20b4f60ae26dfd
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-02-23 14:36:28 +0100

    cleanup before heroku deployment

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,7 +2,6 @@
 <div class="nav-scroller py-1 mb-2">
         <nav class="nav d-flex justify-content-between">
           <a class="p-2 text-muted" href="#">...</a>
-          <a class="p-2 text-muted" href="#">Admin</a>
           <a class="p-2 text-muted" href="/blogs/create">Add Blog</a>
           <a class="p-2 text-muted" href="#">...</a>
         </nav>
